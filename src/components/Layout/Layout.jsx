5aac718f23d64b175f66d5b5b175d61f15caffdf
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-13 19:46:25 +0300

    Доработка компонента Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const Layout = props => {
   return (
     <Container className="pb-4">
-      <h1 className="py-3">GitHub</h1>
-      <main>{props.children}</main>
+      <Row>
+        <Col>
+          <h1 className="py-3">GitHub</h1>
+          <main>{props.children}</main>
+        </Col>
+      </Row>
     </Container>
   );
 };
