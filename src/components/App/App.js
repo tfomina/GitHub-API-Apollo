a979e2563c28c4cefa1f78bbab4503e7fd1360df
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-13 19:46:52 +0300

    Получение данных через fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Layout } from "./../Layout";
+import { List } from "./../List";
 import { Loader } from "./../Loader";
 
 export const App = () => {
-  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const result = await fetch(
+        "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc"
+      );
+      const repositories = await result.json();
+      setData(repositories.items);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Layout>
@@ -12,7 +34,7 @@ export const App = () => {
 
       {isLoading && <Loader />}
 
-      <>Тут будут данные</>
+      {data && !!data.length && <List data={data} />}
     </Layout>
   );
 };
