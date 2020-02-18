d6baf42ec2bed3c135762f7659581e89c597eeb7
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-18 20:26:15 +0300

    Доработка компонента Stars

--- src/components/List/Stars.js
+++ src/components/List/Stars.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ReactComponent as Star } from "../../icons/star.svg";
 
-export const Stars = ({ count }) => (
+export const Stars = ({ count = 0 }) => (
   <div className="star-wrapper">
     <Star className="star" /> {count}
   </div>
