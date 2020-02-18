00c3784ec9ba8627c037618ea4083972b6e53df1
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-18 20:24:29 +0300

    Доработка компонента Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,7 +5,6 @@ export const Search = ({ handleNameSearchChange, nameSearch }) => (
   <div className="flex-grow-1 mx-lg-3 mb-4 mb-lg-0">
     <input
       type="text"
-      name="search"
       placeholder="Поиск по названию репозитория"
       onChange={e => handleNameSearchChange(e.target.value)}
       className="form-control"
